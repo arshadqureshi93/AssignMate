0de46fe4b7d73d0e5f4789775e8f4757fb61eb71
arshadqureshi93 <mo.arshad.93733@gmail.com>
2025-08-24 21:25:58 +0530

    chore: configure app hooks and integration
    - Added app_include_js for JavaScript loading
    - Configured app installation settings
    - Set up basic app metadata and structure

--- assignmate/hooks.py
+++ assignmate/hooks.py
@@ -27,6 +27,9 @@ app_license = "mit"
 # include js, css files in header of desk.html
 # app_include_css = "/assets/assignmate/css/assignmate.css"
 # app_include_js = "/assets/assignmate/js/assignmate.js"
+app_include_js = [
+    "js/assignmate.js"
+]
 
 # include js, css files in header of web template
 # web_include_css = "/assets/assignmate/css/assignmate.css"
